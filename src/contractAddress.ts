5983e94df56ce1d06ca3eafbd3c15af8f2194125
ltran <loris.tran@epfl.ch>
2024-09-16 21:41:02 +0200

    fix: updated to new time contract

--- src/contractAddress.ts
+++ src/contractAddress.ts
@@ -5,5 +5,5 @@ export const PoolModifyLiquidityTestAddress = '0x39BF2eFF94201cfAA471932655404F6
 export const HookAddress = 	'0x4E4a9A0C097427A4A72B1386A031fd411adf8aC0';
 export const MockUSDTAddress ='0x0f1D1b7abAeC1Df25f2C4Db751686FC5233f6D3f';
 export const MockFUSDAddress = '0xc7c06a77b481869ecc57E5432D03c3661406424D';
-export const TimeSlotSystemAddress = '0x003280827CfCEE0d0399a95FFcFF6d1195D4e461';
-export const MockERC721Address = '0x609573b2f4459b9bb9B37E4c3ee4F02f4ED53695';
+export const TimeSlotSystemAddress = '0x527A1ceA21F996F0990F977602B62371a37c71DC';
+export const MockERC721Address = '0x5E17291436a4a87DFA9Cc75f79174E5Ff587B753';
